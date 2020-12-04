8d86462f8638828a2f42e6f2fcd8a1b76098b8be
Tianquan Guo <tianquanguo@gmail.com>
2020-12-03 16:47:20 -0800

    Added static Strings to Twitchclient for stream, video, and clips api address.

--- src/main/java/com/tianquan/jupiter/external/TwitchClient.java
+++ src/main/java/com/tianquan/jupiter/external/TwitchClient.java
@@ -22,6 +22,11 @@ public class TwitchClient {
     private static final String TOP_GAME_URL_TEMPLATE = "https://api.twitch.tv/helix/games/top?first=%s";
     private static final String GAME_SEARCH_URL_TEMPLATE = "https://api.twitch.tv/helix/games?name=%s";
     private static final int DEFAULT_GAME_LIMIT = 20;
+    private static final String STREAM_SEARCH_URL_TEMPLATE = "https://api.twitch.tv/helix/streams?game_id=%s&first=%s";
+    private static final String VIDEO_SEARCH_URL_TEMPLATE = "https://api.twitch.tv/helix/videos?game_id=%s&first=%s";
+    private static final String CLIP_SEARCH_URL_TEMPLATE = "https://api.twitch.tv/helix/clips?game_id=%s&first=%s";
+    private static final String TWITCH_BASE_URL = "https://www.twitch.tv/";
+    private static final int DEFAULT_SEARCH_LIMIT = 20;
 
     private String searchTwitch(String url) throws TwitchException {
         CloseableHttpClient httpclient = HttpClients.createDefault();
