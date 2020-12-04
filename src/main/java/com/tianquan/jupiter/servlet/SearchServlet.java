31989645fa6aa0a442a70af484d345f7b90ac072
Tianquan Guo <tianquanguo@gmail.com>
2020-12-03 17:37:39 -0800

    Added SearchServlet, and changed doGet method.

---
+++ src/main/java/com/tianquan/jupiter/servlet/SearchServlet.java
@@ -0,0 +1,35 @@
+package com.tianquan.jupiter.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import com.tianquan.jupiter.external.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@WebServlet(name = "SearchServlet", urlPatterns = {"/search"})
+public class SearchServlet extends HttpServlet {
+//    We don't need Post method for current purpose
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//
+//    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String gameId = request.getParameter("game_id");
+        if (gameId == null) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        TwitchClient client = new TwitchClient();
+        try {
+            response.setContentType("application/json;charset=UTF-8");
+            response.getWriter().print(new ObjectMapper().writeValueAsString(client.searchItems(gameId)));
+        } catch (TwitchException e) {
+            throw new ServletException(e);
+        }
+
+    }
+}
