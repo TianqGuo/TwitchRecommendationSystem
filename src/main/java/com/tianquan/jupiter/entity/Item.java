42172e0db59d1c903a0cf375d508c771342d9202
Tianquan Guo <tianquanguo@gmail.com>
2020-12-03 18:51:09 -0800

    Fixed some bugs, JsonDeserialize problem has been solved after adding the related annotations.

--- src/main/java/com/tianquan/jupiter/entity/Item.java
+++ src/main/java/com/tianquan/jupiter/entity/Item.java
@@ -4,7 +4,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonDeserialize(builder = Item.Builder.class)
 public class Item {
     @JsonProperty("id")
     private final String id;
